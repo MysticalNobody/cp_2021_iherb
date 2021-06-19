2eb5c4741d6bb216b508084950ed4619dcd1515b
Mikhail Kolchanov <01gicha01@gmail.com>
2021-06-19 19:01:10 +0300

    made paralax

--- lib/presentation/features/content/widgets/article_card.dart
+++ lib/presentation/features/content/widgets/article_card.dart
@@ -1,74 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:iherb/data/models/article_model.dart';
+import 'package:iherb/presentation/widgets/paralax_image.dart';
 
 class ArticleCard extends StatelessWidget {
-  const ArticleCard(this.article);
+  ArticleCard(this.article);
 
   final ArticleModel article;
 
   String get tags => article.tags.join(' • ').toUpperCase();
 
+  final GlobalKey _backgroundImageKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(26),
-      child: Stack(
-        children: [
-          Image(
-            fit: BoxFit.fitWidth,
-            image: NetworkImage(article.photo),
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.transparent,
-                  Colors.black,
-                ],
-              ),
-            ),
-            child: Opacity(
-              opacity: 0,
-              child: Image(
-                fit: BoxFit.fitWidth,
-                image: NetworkImage(article.photo),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 23,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 22),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width - 96,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tags,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white.withOpacity(.8),
-                        fontFamily: "Arial",
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      article.title,
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.white,
-                        fontFamily: "ArialBlack",
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
+      child: SizedBox(
+        child: Stack(
+          children: [
+            _buildParallaxBackground(context, _backgroundImageKey),
+            DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.transparent,
+                    Colors.black,
                   ],
                 ),
               ),
+              child: Opacity(
+                opacity: 0,
+                child: _buildParallaxBackground(context, GlobalKey()),
+              ),
             ),
+            Positioned(
+              bottom: 23,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 22),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width - 96,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tags,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white.withOpacity(.8),
+                          fontFamily: "Arial",
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        article.title,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                          fontFamily: "ArialBlack",
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildParallaxBackground(BuildContext context, GlobalKey key) {
+    return AspectRatio(
+      aspectRatio: 14 / 9,
+      child: Flow(
+        delegate: ParallaxFlowDelegate(
+          scrollable: Scrollable.of(context)!,
+          listItemContext: context,
+          backgroundImageKey: key,
+        ),
+        children: [
+          Image.network(
+            article.photo,
+            key: key,
+            fit: BoxFit.fitWidth,
           ),
         ],
       ),
