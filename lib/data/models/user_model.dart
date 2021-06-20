9f8a6d341c91fa32973a9cc8c58f6f1efa46c8af
Mikhail Kolchanov <01gicha01@gmail.com>
2021-06-20 04:19:27 +0300

    remade user model

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -5,10 +5,30 @@ part 'user_model.g.dart';
 @JsonSerializable()
 class UserModel {
   final String id;
-  String? email;
-  String? imageUrl;
+  String? feel;
+  String? lifestyle;
+  String? diet;
+  String? sex;
+  bool? pregnant;
+  int? age;
+  int? weight;
+  int? height;
+  List<String>? allergy;
+  String? chronic;
 
-  UserModel({required this.id, this.email,this.imageUrl});
+  UserModel({
+    required this.id,
+    this.feel,
+    this.lifestyle,
+    this.diet,
+    this.sex,
+    this.pregnant,
+    this.age,
+    this.weight,
+    this.height,
+    this.allergy,
+    this.chronic,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
   Map<String, dynamic> toJson() => _$UserModelToJson(this);
--- lib/data/models/user_model.g.dart
+++ lib/data/models/user_model.g.dart
@@ -9,13 +9,30 @@ part of 'user_model.dart';
 UserModel _$UserModelFromJson(Map<String, dynamic> json) {
   return UserModel(
     id: json['id'] as String,
-    email: json['email'] as String?,
-    imageUrl: json['imageUrl'] as String?,
+    feel: json['feel'] as String?,
+    lifestyle: json['lifestyle'] as String?,
+    diet: json['diet'] as String?,
+    sex: json['sex'] as String?,
+    pregnant: json['pregnant'] as bool?,
+    age: json['age'] as int?,
+    weight: json['weight'] as int?,
+    height: json['height'] as int?,
+    allergy:
+        (json['allergy'] as List<dynamic>?)?.map((e) => e as String).toList(),
+    chronic: json['chronic'] as String?,
   );
 }
 
 Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
       'id': instance.id,
-      'email': instance.email,
-      'imageUrl': instance.imageUrl,
+      'feel': instance.feel,
+      'lifestyle': instance.lifestyle,
+      'diet': instance.diet,
+      'sex': instance.sex,
+      'pregnant': instance.pregnant,
+      'age': instance.age,
+      'weight': instance.weight,
+      'height': instance.height,
+      'allergy': instance.allergy,
+      'chronic': instance.chronic,
     };
