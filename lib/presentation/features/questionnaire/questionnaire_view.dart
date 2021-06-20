c3af8654e7b9ec6cad24b691d677ae85cf53c43c
Nikita Khilobok <nikita.khilobok@umbrellait.com>
2021-06-20 04:18:15 +0300

    init questionnaire

---
+++ lib/presentation/features/questionnaire/questionnaire_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuestionnaireView extends StatelessWidget {
+  const QuestionnaireView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
